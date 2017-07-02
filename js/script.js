2d1bc8c7ffe025f566125049f91a36e077b30191
phillip michael townshend <845862126@ken20-202-18.local>
2017-07-03 11:17:46 +1200

    added accordion drop downs, fixed multiple of same locations

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
+$( function() {
+    $( "#accordion" ).accordion({
+      collapsible: true
+    });
+  });
+
 var marker;
-var currentMarker;
+var currentMarker = 'none';
 var markers = [];
+var currentMarker;
+var transportMode = "DRIVING";
+var directionDisplay;
+var userLocation;
 var mapMarkers = [
 	{
 		lat: -41.296529,
@@ -99,15 +109,66 @@ function addMarkers(){
 		})
 		markers.push(marker);
 		clickMarker(marker);
-		console.log(marker);
 	};
 }
 
 function clickMarker(marker) {
 			google.maps.event.addListener(marker, "click", function(){
 			$("#getstarted").hide();
-			$("#locations").append("<div class='locationdetails'><strong>"+marker.title+"</strong></div>")
-			// currentMarker = marker;
+			if(marker.title == currentMarker.title){
+			} else {
+				$("#locations").append("<div id='accordion' class='locationdetails'><strong>"
+																+marker.title+"</strong><div id='dropdown'><p>Lorem ipsum dolor sit amet</p></div></div>")
 
+																$( "#accordion" ).accordion({
+														      collapsible: true
+														    });
+
+				currentMarker = marker;
+				marker.setIcon("img/house.png");
+				marker.setAnimation(google.maps.Animation.BOUNCE);
+			// showDirection(currentMarker.position, transportMode);
+			}
 			});
 }
+
+// function showDirection(location, mode){
+// 	//If there is already a direction line on the map then remove it
+// 	if(directionDisplay){
+// 		directionDisplay.setMap(null);
+// 	}
+// 	//Create a new instance of DirectionsService
+// 	var directionService = new google.maps.DirectionsService();
+// 	//Create a new instance of DirectionRendere
+// 	//This draws the lines on the map
+// 	//This was also initialised at the top of the page
+// 	directionDisplay = new google.maps.DirectionsRenderer();
+//
+// 	//set what map you want it to show on
+// 	directionDisplay.setMap(map);
+//
+// 	//The DirectionService only needs origin, destination and travelMode, but there are several other options you can add
+// 	directionService.route({
+// 		//What is the starting place (lat/lng)
+// 		origin: userLocation.position,
+// 		//What is the end place (lat/lng)
+// 		destination: currentMarker.position,
+// 		//How is the user getting there
+// 		travelMode: google.maps.TravelMode[mode],
+// 	}, function(response, status){
+// 		//When it comes back from the server you will get a response and a status
+// 		//you should write a case for all of the different status
+// 		//Have a look at the Google Maps API for all of them
+//
+// 		//If everything is all good
+// 		if(status == "OK"){
+// 			//Show the directions on the map
+// 			directionDisplay.setDirections(response);
+// 		} else if(status == "NOT_FOUND"){
+// 			//If one of the start or end locations werent found
+//
+// 		} else if(status == "ZERO_RESULTS"){
+// 			//If there is no results of how to get to the locations
+// 		}
+// 	});
+// }
